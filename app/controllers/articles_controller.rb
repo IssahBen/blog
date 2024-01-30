6bb53ee0ec0cb505414ba4f65bd077ea77afaaa7
IssahBen <benedictissah@gmail.com>
2024-01-30 01:29:02 -0600

    web blog app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new 
+        @article = Article.new
+    end 
+
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            flash[:notice] =  "article was created "
+            redirect_to articles_path
+
+        else
+            render :new ,status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        @article.update(article_params)
+
+        redirect_to article_path(@article)
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+
+
+
+end
