a84f8537287c3938b1c14d7ac6c1fc4dc356233e
IssahBen <benedictissah@gmail.com>
2024-02-01 00:11:26 -0600

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require 'bcrypt'
 class User < ApplicationRecord
+
+    before_save {self.email=email.downcase}
 
     validates :username ,presence: true ,uniqueness: {case_sensitive: false},length: {minimum: 3 ,maximum: 25}
 
@@ -7,4 +10,6 @@ class User < ApplicationRecord
     validates :email ,presence: true ,uniqueness: {case_sensitive: false},length: {maximum: 105},format: {with: VALID_EMAIL_REGEX}
 
     has_many :articles
+
+    has_secure_password
 end
